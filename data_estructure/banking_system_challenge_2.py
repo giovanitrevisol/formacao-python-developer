8452cc6a84b1f7cc98f4a2a10bb2d75928a3a766
Giovani <giovani.trevisol@cwi.com.br>
2023-05-19 16:38:08 -0300

    estrutura de dados com python

--- data_estructure/banking_system_challenge_2.py
+++ data_estructure/banking_system_challenge_2.py
@@ -4,13 +4,13 @@ import textwrap
 def menu():
     menu = """\n
     ================ MENU ================
-    [d]\tDepositar
-    [s]\tSacar
-    [e]\tExtrato
-    [nc]\tNova conta
-    [lc]\tListar contas
-    [nu]\tNovo usuário
-    [q]\tSair
+    [d]\tDepositar.
+    [s]\tSacar.
+    [e]\tExtrato.
+    [nc]\tNova conta.
+    [lc]\tListar contas.
+    [nu]\tNovo usuário.
+    [q]\tSair.
     => """
     return input(textwrap.dedent(menu))
 
